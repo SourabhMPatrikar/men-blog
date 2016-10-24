64681685a75900ec98b6aa83aa7bcb510cc7fa87
Sourabh.p <Sourabh.p@PM-ISML.COM>
2016-10-24 16:28:22 +0530

    made one new api with name sourabh:- temp api

--- routes/index.js
+++ routes/index.js
@@ -14,11 +14,17 @@ router.get('/post/:post_id', function(req, res, next) {
   res.render('postdetails', { title: 'Express' });
 });
 
-
+//router.get('/category', function(req, res, next){
+//	res.render('category',{title:'Express'});
+//});
+//
+//router.get('/category/:category_id',function(req, res, next){
+//	res.render('categorydetails',{title:'Express'});
+//});
 
 router.get('/api/category', function(req, res, next) {
   var db = req.db;
-  db.collection('Category').find({"category_id" : "2"}).toArray(function(err, docs) {
+  db.collection('Category').find({"id" : "2"}).toArray(function(err, docs) {
 	  console.log(docs);
 	  if(err || !docs.length){
 		  res.json({
@@ -31,5 +37,29 @@ router.get('/api/category', function(req, res, next) {
   });  
 });
 
+router.get('/api/post', function(req, res, next){
+	var db = req.db;
+	db.collection('Post').find({"title" : "Zee TV"}).toArray(function(err, docs){
+		console.log(docs);
+		if(err || !docs.length){
+			res.json({success:false, err:err});
+		}
+		else{
+			res.json(docs);
+		}
+	});
+});
 
+router.get('/sourabh', function(req, res, next){
+	var db = req.db;
+	db.collection('Post').find().toArray(function(err, docs){
+		console.log(docs);
+		if(err || !docs.length){
+			res.json({success:false,err:err});
+		}
+		else{
+			res.json(docs);
+		}
+	});
+});
 module.exports = router;
